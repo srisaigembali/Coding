9da2da8353dbefd2450d1de314930c134bdf9912
srisaigembali <70004293+srisaigembali@users.noreply.github.com>
2024-10-23 16:26:03 +0530

    loading in nextjs

--- Next JS/next-app-1/app/page.tsx
+++ Next JS/next-app-1/app/page.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 async function getUserDetails() {
+	await new Promise((r) => setTimeout(r, 5000));
 	const response = await axios.get(
 		"https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
 	);
@@ -11,9 +12,14 @@ export default async function Home() {
 	const userData = await getUserDetails();
 
 	return (
-		<div>
-			{userData.email}
-			{userData.name}
+		<div className='flex flex-col justify-center h-screen'>
+			<div className='flex justify-center'>
+				<div className='border p-8 rounded'>
+					<div>Name: {userData?.name}</div>
+
+					{userData?.email}
+				</div>
+			</div>
 		</div>
 	);
 }
